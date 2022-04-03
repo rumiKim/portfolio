4e00137fcdc3a55d2b8ffe91caaa394720e5ee6c
AhreumKim <ahreum.kim38@gmail.com>
2022-04-03 23:32:10 +0900

    220403

---
+++ common/js/slide-m.js
@@ -0,0 +1,9 @@
+$(function(){
+  const swiper1 = new Swiper(".sec_visual .swiper-container", {
+    pagination: {
+          el: ".swiper-pagination",
+          type:'fraction'
+        },
+  });
+
+});
