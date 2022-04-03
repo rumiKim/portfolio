3db89a53275e656f1a9cc12521f3255b3e3a1e08
AhreumKim <ahreum.kim38@gmail.com>
2022-04-03 20:05:57 +0900

    220403

--- common/js/mobile.js
+++ common/js/mobile.js
@@ -1,18 +1,43 @@
-$(function(){
+$(function() {
 
-  $('.sel_link').on('change',function(){
-    let url = $(this).val();
-    location.href = url
-  });
+      // 메뉴
+      const siteMap = $('.sitemap'),
+        gnbListItem = $('.gnb_total li > a');
 
 
-  $('.list_faq > li > a').on('click',function(e){
-    e.preventDefault()
-    $(this).next().stop().slideToggle(200);
-    $(this).parent().toggleClass('active');
-    $(this).parent().siblings().removeClass('active');
-    $(this).parent().siblings().children('div').slideUp(200);
+      $('.btn_menu').on('click', function() {
+        siteMap.toggleClass('active');
+      });
 
-  });
+      gnbListItem.on('click',function(e){
+        if($(this).next().length > 0){
+          e.preventDefault();
+          $(this).toggleClass('active');
+          $(this).parent().siblings().children('a').removeClass('active')
+        }
+      });
 
-});
+
+
+
+
+        // select 페이지 이동
+        $('.sel_link').on('change', function() {
+          let url = $(this).val();
+          location.href = url
+        });
+
+
+        // 자주묻는 질문 드롭다운 리스트
+        $('.list_faq > li > a').on('click', function(e) {
+          e.preventDefault()
+          $(this).next().stop().slideToggle(200);
+          $(this).parent().toggleClass('active');
+          $(this).parent().siblings().removeClass('active');
+          $(this).parent().siblings().children('div').slideUp(200);
+        });
+
+
+
+
+      }); //jquery function end
