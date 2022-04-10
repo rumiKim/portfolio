61dc145822c0e01aeab9ace1516f62f87891ae0a
AhreumKim <ahreum.kim38@gmail.com>
2022-04-10 22:12:47 +0900

    220410

--- common/js/slide-m.js
+++ common/js/slide-m.js
@@ -1,9 +1,25 @@
 $(function(){
+
+  // 메인 비주얼
   const swiper1 = new Swiper(".sec_visual .swiper-container", {
     pagination: {
           el: ".swiper-pagination",
           type:'fraction'
         },
   });
+
+
+  const swiper2 = new Swiper(".sec_notice .swiper-container", {
+    direction: "vertical",
+    navigation: {
+          nextEl: ".sec_notice .btn_next",
+          prevEl: ".sec_notice .btn_prev",
+        },
+    loop:true
+  });
+
+
+
+
 
 });
